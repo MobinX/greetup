945bc742282bcd9b76244874931ae658f4fa012a
Mobin Chowdhury <47663360+MobinX@users.noreply.github.com>
2024-02-01 13:47:10 +0000

    dd

--- candle-kernels/build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    println!("cargo:rerun-if-changed=build.rs");
-
-    let builder = bindgen_cuda::Builder::default();
-    println!("cargo:info={builder:?}");
-    let bindings = builder.build_ptx().unwrap();
-    bindings.write("src/lib.rs").unwrap();
-}
